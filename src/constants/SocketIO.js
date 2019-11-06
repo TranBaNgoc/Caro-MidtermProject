da3632209ac2054004c449efe31d5d1863101895
TranBaNgoc <tbngoc.khtn@gmail.com>
2019-11-06 20:54:09 +0700

    Fix host socket.IO

--- src/constants/SocketIO.js
+++ src/constants/SocketIO.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
 
-// export default io.connect('https://btcn06-1612431.herokuapp.com/');
+export default io.connect('https://btcn06-1612431.herokuapp.com/');
 
-export default io.connect('localhost:4000');
+// export default io.connect('localhost:4000');
